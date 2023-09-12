92d5ee5795deecaee71861a25e69be5c69248e38
Suryanka Ghosh <120125240+suryanka@users.noreply.github.com>
2023-09-12 22:02:34 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -22,8 +22,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/',router);
 app.use(express.json({ extended: false }));
 
-const username= process.env.DB_USERNAME;
-const password= process.env.DB_PASSWORD;
+const username= process.env.MONGO_USERNAME;
+const password= process.env.MONGO_PASSWORD;
 const url = `mongodb+srv://${username}:${password}@myntra-clone.5bpuxfp.mongodb.net/?retryWrites=true&w=majority
 `;
 
