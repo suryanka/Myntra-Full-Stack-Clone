5fbf0ea578f55c85193a0dfab5e1b3a2ae2ccd7f
Suryanka Ghosh <120125240+suryanka@users.noreply.github.com>
2023-09-12 21:05:23 +0530

    Update api.js

--- server/client/src/Services/api.js
+++ server/client/src/Services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const url= 'http://localhost:8000';
+const url= 'https://vercel.com/suryanka/myntra-full-stack-clone-backend';
 
 export const authenticateLogin= async (data)=> {
 
