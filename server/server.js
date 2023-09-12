f499754e172703bb173f85ee15f80289f511399b
Suryanka Ghosh <120125240+suryanka@users.noreply.github.com>
2023-09-12 21:59:58 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -24,7 +24,8 @@ app.use(express.json({ extended: false }));
 
 const username= process.env.DB_USERNAME;
 const password= process.env.DB_PASSWORD;
-const url = `mongodb+srv://${username}:${password}@myntra-clone.5bpuxfp.mongodb.net/`;
+const url = `mongodb+srv://${username}:${password}@myntra-clone.5bpuxfp.mongodb.net/?retryWrites=true&w=majority
+`;
 
 connection(url);
 const port = process.env.port || 8000;
