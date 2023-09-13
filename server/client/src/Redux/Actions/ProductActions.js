947ae1c05b06027bb07951aecec89a2b3a6612ab
Suryanka Ghosh <120125240+suryanka@users.noreply.github.com>
2023-09-13 21:04:55 +0530

    Update ProductActions.js

--- server/client/src/Redux/Actions/ProductActions.js
+++ server/client/src/Redux/Actions/ProductActions.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import * as actiontypes from "../Constants/productConstant.js";
 
-const url = "https://myntra-full-stack-clone-backend.vercel.app/";
+const url = "https://myntra-full-stack-clone-backend.vercel.app";
 
 export const getProducts = () => async (dispatch) => {
   try {
